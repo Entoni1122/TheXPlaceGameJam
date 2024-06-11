317351b3e22f7faf220c83d0ff8908c853615dd8
DevAlexii <a.d.s100698@gmail.com>
2024-06-11 11:25:20 +0200

    Add setupscene

---
+++ Assets/_Script/SceneLoaderScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoaderScript : MonoBehaviour
+{
+     void Start()
+    {
+        StartCoroutine(LoadMainScene());
+    }
+
+    IEnumerator LoadMainScene()
+    {
+        yield return new WaitUntil(() => NetworkManager.Singleton != null);
+        SceneManager.LoadScene("Entoni");
+    }
+}
