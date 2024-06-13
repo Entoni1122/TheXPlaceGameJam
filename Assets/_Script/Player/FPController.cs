8e3b349cfc74bb84ceface11b240d014c263e078
Entoni1122 <entonishkalla34@gmail.com>
2024-06-13 18:14:43 +0200

    FPS Controller

---
+++ Assets/_Script/Player/FPController.cs
@@ -0,0 +1,428 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    #region Camera Movement Variables
+
+    public Camera playerCamera;
+    [Header("CameraMovement")]
+    [SerializeField] float fov = 60f;
+    [SerializeField] bool invertCamera = false;
+    [SerializeField] bool cameraCanMove = true;
+    [SerializeField] float mouseSensitivity = 2f;
+    [SerializeField] float maxLookAngle = 50f;
+
+    [SerializeField] bool lockCursor = true;
+    [SerializeField] bool crosshair = true;
+    [SerializeField] Sprite crosshairImage;
+    [SerializeField] Color crosshairColor = Color.white;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private Image crosshairObject;
+
+    [Header("CameraZoom")]
+    [SerializeField] bool enableZoom = true;
+    [SerializeField] bool holdToZoom = false;
+    [SerializeField] KeyCode zoomKey = KeyCode.Mouse1;
+    [SerializeField] float zoomFOV = 30f;
+    [SerializeField] float zoomStepTime = 5f;
+    private bool isZoomed = false;
+
+    float caeraXRotInput;
+    float caeraYRotInput;
+    #endregion
+
+    #region Movement Variables
+    [Header("PlayerMovement")]
+    [SerializeField] bool playerCanMove = true;
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float maxVelocityChange = 10f;
+
+    private bool isWalking = false;
+
+    #region Sprint
+    [Header("SprintVariables")]
+    [SerializeField] bool enableSprint = true;
+    [SerializeField] bool unlimitedSprint = false;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] float sprintSpeed = 7f;
+    [SerializeField] float sprintDuration = 5f;
+    [SerializeField] float sprintCooldown = .5f;
+    [SerializeField] float sprintFOV = 80f;
+    [SerializeField] float sprintFOVStepTime = 10f;
+    [SerializeField] bool useSprintBar = true;
+    [SerializeField] Image sprintBarBG;
+    [SerializeField] Image sprintBar;
+    private bool isSprinting = false;
+    private float sprintRemaining;
+    private bool isSprintCooldown = false;
+    private float sprintCooldownReset;
+
+    #endregion
+
+    #region Jump
+    [Header("Jump")]
+    [SerializeField] bool enableJump = true;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] float jumpPower = 5f;
+
+    private bool isGrounded = false;
+
+    #endregion
+
+    #region Crouch
+    [Header("CrouchVariables")]
+    [SerializeField] bool enableCrouch = true;
+    [SerializeField] bool holdToCrouch = true;
+    [SerializeField] KeyCode crouchKey = KeyCode.LeftControl;
+    [SerializeField] float crouchHeight = .75f;
+    [SerializeField] float speedReduction = .5f;
+
+    private bool isCrouched = false;
+    private Vector3 originalScale;
+
+    #endregion
+    #endregion
+
+    #region Head Bob
+    [Header("HeadBounce")]
+    [SerializeField] bool enableHeadBob = true;
+    [SerializeField] Transform joint;
+    [SerializeField] float bobSpeed = 10f;
+    [SerializeField] Vector3 bobAmount = new Vector3(.15f, .05f, 0f);
+
+    private Vector3 jointOriginalPos;
+    private float timer = 0;
+
+    #endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        crosshairObject = GetComponentInChildren<Image>();
+
+        playerCamera.fieldOfView = fov;
+        originalScale = transform.localScale;
+        jointOriginalPos = joint.localPosition;
+        if (!unlimitedSprint)
+        {
+            sprintRemaining = sprintDuration;
+            sprintCooldownReset = sprintCooldown;
+        }
+    }
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (crosshair)
+        {
+            crosshairObject.sprite = crosshairImage;
+            crosshairObject.color = crosshairColor;
+        }
+        else
+        {
+            crosshairObject.gameObject.SetActive(false);
+        }
+    }
+
+    float camRotation;
+
+    private void Update()
+    {
+        ReadInput();
+        CameraBehaviour();
+        SprintCheck();
+
+        if (enableJump && Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+
+        CrouchCheck();
+        CheckGround();
+
+        if (enableHeadBob)
+        {
+            HeadBob();
+        }
+    }
+
+
+    void ReadInput()
+    {
+        caeraXRotInput = Input.GetAxis("Mouse X");
+        caeraYRotInput = Input.GetAxis("Mouse Y");
+    }
+    void CameraBehaviour()
+    {
+        if (cameraCanMove)
+        {
+            yaw = transform.localEulerAngles.y + caeraXRotInput * mouseSensitivity;
+
+            if (!invertCamera)
+            {
+                pitch -= mouseSensitivity * caeraYRotInput;
+            }
+            else
+            {
+                // Inverted Y
+                pitch += mouseSensitivity * caeraYRotInput;
+            }
+
+            pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+            transform.localEulerAngles = new Vector3(0, yaw, 0);
+            playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+        }
+
+        //zoom
+        if (enableZoom)
+        {
+            if (Input.GetKeyDown(zoomKey) && !holdToZoom && !isSprinting)
+            {
+                if (!isZoomed)
+                {
+                    isZoomed = true;
+                }
+                else
+                {
+                    isZoomed = false;
+                }
+            }
+
+            if (holdToZoom && !isSprinting)
+            {
+                if (Input.GetKeyDown(zoomKey))
+                {
+                    isZoomed = true;
+                }
+                else if (Input.GetKeyUp(zoomKey))
+                {
+                    isZoomed = false;
+                }
+            }
+
+            if (isZoomed)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, zoomFOV, zoomStepTime * Time.deltaTime);
+            }
+            else if (!isZoomed && !isSprinting)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, fov, zoomStepTime * Time.deltaTime);
+            }
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    #region Checks
+    void SprintCheck()
+    {
+        if (enableSprint)
+        {
+            if (isSprinting)
+            {
+                isZoomed = false;
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, sprintFOV, sprintFOVStepTime * Time.deltaTime);
+
+                if (!unlimitedSprint)
+                {
+                    sprintRemaining -= 1 * Time.deltaTime;
+                    if (sprintRemaining <= 0)
+                    {
+                        isSprinting = false;
+                        isSprintCooldown = true;
+                    }
+                }
+            }
+            else
+            {
+                sprintRemaining = Mathf.Clamp(sprintRemaining += 1 * Time.deltaTime, 0, sprintDuration);
+            }
+
+            if (isSprintCooldown)
+            {
+                sprintCooldown -= 1 * Time.deltaTime;
+                if (sprintCooldown <= 0)
+                {
+                    isSprintCooldown = false;
+                }
+            }
+            else
+            {
+                sprintCooldown = sprintCooldownReset;
+            }
+
+            if (useSprintBar && !unlimitedSprint)
+            {
+                float sprintRemainingPercent = sprintRemaining / sprintDuration;
+                sprintBar.transform.localScale = new Vector3(sprintRemainingPercent, 1f, 1f);
+            }
+        }
+    }
+    void CrouchCheck()
+    {
+        if (enableCrouch)
+        {
+            if (Input.GetKeyDown(crouchKey) && !holdToCrouch)
+            {
+                Crouch();
+            }
+
+            if (Input.GetKeyDown(crouchKey) && holdToCrouch)
+            {
+                isCrouched = false;
+                Crouch();
+            }
+            else if (Input.GetKeyUp(crouchKey) && holdToCrouch)
+            {
+                isCrouched = true;
+                Crouch();
+            }
+        }
+    }
+    private void CheckGround()
+    {
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        float distance = .75f;
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
+        {
+            Debug.DrawRay(origin, direction * distance, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion
+
+    #region MVM
+    void Movement()
+    {
+        if (playerCanMove)
+        {
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            if (targetVelocity.x != 0 || targetVelocity.z != 0 && isGrounded)
+            {
+                isWalking = true;
+            }
+            else
+            {
+                isWalking = false;
+            }
+
+            if (enableSprint && Input.GetKey(sprintKey) && sprintRemaining > 0f && !isSprintCooldown)
+            {
+                targetVelocity = transform.TransformDirection(targetVelocity) * sprintSpeed;
+
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                if (velocityChange.x != 0 || velocityChange.z != 0)
+                {
+                    isSprinting = true;
+
+                    if (isCrouched)
+                    {
+                        Crouch();
+                    }
+
+                }
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+            else
+            {
+                isSprinting = false;
+
+                targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+        }
+    }
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (isCrouched && !holdToCrouch)
+        {
+            Crouch();
+        }
+    }
+    private void Crouch()
+    {
+        if (isCrouched)
+        {
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+            walkSpeed /= speedReduction;
+
+            isCrouched = false;
+        }
+        else
+        {
+            transform.localScale = new Vector3(originalScale.x, crouchHeight, originalScale.z);
+            walkSpeed *= speedReduction;
+
+            isCrouched = true;
+        }
+    }
+    private void HeadBob()
+    {
+        if (isWalking)
+        {
+            if (isSprinting)
+            {
+                timer += Time.deltaTime * (bobSpeed + sprintSpeed);
+            }
+            else if (isCrouched)
+            {
+                timer += Time.deltaTime * (bobSpeed * speedReduction);
+            }
+            else
+            {
+                timer += Time.deltaTime * bobSpeed;
+            }
+            joint.localPosition = new Vector3(jointOriginalPos.x + Mathf.Sin(timer) * bobAmount.x, jointOriginalPos.y + Mathf.Sin(timer) * bobAmount.y, jointOriginalPos.z + Mathf.Sin(timer) * bobAmount.z);
+        }
+        else
+        {
+            timer = 0;
+            joint.localPosition = new Vector3(Mathf.Lerp(joint.localPosition.x, jointOriginalPos.x, Time.deltaTime * bobSpeed), Mathf.Lerp(joint.localPosition.y, jointOriginalPos.y, Time.deltaTime * bobSpeed), Mathf.Lerp(joint.localPosition.z, jointOriginalPos.z, Time.deltaTime * bobSpeed));
+        }
+    }
+    #endregion
+
+}
+
